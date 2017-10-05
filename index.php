b78d77d88e072492412f6b4b9c8a77fd246ff808
Tobias Hvass Mølbak <thm@combine.dk>
2017-10-05 17:07:51 +0200

    Added to git

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <title>Ettrup Molbak Bryllup</title>
+  
+  <link rel="shortcut icon" href="/images/favicon.ico"/>
+  <link rel="stylesheet" type="text/css" href="dist/css/bootstrap.min.css">
+  <!-- Optional Bootstrap theme -->
+  <link rel="stylesheet" href="dist/css/custom-bootstrap.css">
+</head>
+
+<body>
+  <?php include 'includes/upper-body.php';?>
+
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Gæstelisten</h1>
+        <p>Se hvem der skal med til årets fest!</p>
+        <p><a class="btn btn-default btn-lg" href="/gaestelisten" role="button">Gæsterne</a></p>
+      </div>
+    </div>
+  
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Ønskesedlen</h1>
+        <p>...</p>
+        <p><a class="btn btn-default btn-lg" href="/oenskesedlen" role="button">Sedlen</a></p>
+      </div>
+    </div>
+
+  <?php include 'includes/lower-body.php';?>
+</body>
+</html>
