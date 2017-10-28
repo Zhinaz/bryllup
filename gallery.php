d68b561979713b5e48a921277f15ff1a97aa9193
Tobias Hvass Mølbak <thm@combine.dk>
2017-10-28 13:26:39 +0200

    Updated gallery

--- gallery.php
+++ gallery.php
@@ -1,71 +1,167 @@
 <!DOCTYPE html><html class="no-js">
-	<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	
-    <?php include 'includes/settings.php';?>
-	
-		<div class="fh5co-hero" data-section="home">
-			<div class="fh5co-overlay"></div>
-			<div class="fh5co-cover text-center" data-stellar-background-ratio="0.5" style="background-image: url(images/background-4.jpg);">
-				<div class="display-t">
-					<div class="display-tc">
-						<div class="container">
-							<div class="col-md-10 col-md-offset-1">
-								<div class="animate-box">
-									<h1>#mølbakbryllup18</h1>
-									<h2>Bryllup's galleriet</h2>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-		<?php include 'includes/header.php';?>
-		<!-- end:header-top -->
-        
-      
-      <div id="fh5co-gallery">
+    <?php include 'includes/settings.php'; ?>
+
+  <div class="fh5co-hero" data-section="home">
+    <div class="fh5co-overlay"></div>
+    <div class="fh5co-cover text-center" data-stellar-background-ratio="0.5" style="background-image: url(images/background-4.jpg);">
+      <div class="display-t">
+        <div class="display-tc">
           <div class="container">
-              <div class="row">
-                  <div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
-                      <h2>Bryllup's galleriet</h2>
-                  </div>
+            <div class="col-md-10 col-md-offset-1">
+              <div class="animate-box">
+                <h1>#mølbakbryllup18</h1>
+                <h2>Bryllup's galleriet</h2>
               </div>
-              <div class="row">
-                  <div class="col-md-4">
-                      <div class="gallery animate-box">
-                          <a class="gallery-img image-popup image-popup" href="gallery-images/gallery-1.jpg"><img src="gallery-images/gallery-1.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
-                      </div>
-                      <div class="gallery animate-box">
-                          <a class="gallery-img image-popup" href="gallery-images/gallery-7.jpg"><img src="gallery-images/gallery-7.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
-                      </div>
-                  </div>
-                  <div class="col-md-4">
-                      <div class="gallery animate-box">
-                          <a class="gallery-img image-popup" href="gallery-images/gallery-6.jpg"><img src="gallery-images/gallery-6.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
-                      </div>
-                      <div class="gallery animate-box">
-                          <a class="gallery-img image-popup" href="gallery-images/gallery-2.jpg"><img src="gallery-images/gallery-2.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
-                      </div>
-                      <div class="gallery animate-box">
-                          <a class="gallery-img image-popup" href="gallery-images/gallery-5.jpg"><img src="gallery-images/gallery-5.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
-                      </div>
-                  </div>
-                  <div class="col-md-4">
-                      <div class="gallery animate-box">
-                          <a class="gallery-img image-popup" href="gallery-images/gallery-3.jpg"><img src="gallery-images/gallery-3.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
-                      </div>
-                      <div class="gallery animate-box">
-                          <a class="gallery-img image-popup" href="gallery-images/gallery-4.jpg"><img src="gallery-images/gallery-4.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
-                      </div>
-                  </div>
-              </div>
+            </div>
           </div>
+        </div>
       </div>
-		<?php include 'includes/footer.php';?>
-	</body>
+    </div>
+  </div>
+
+  <?php include 'includes/header.php'; ?>
+  <!-- end:header-top -->
+
+
+  <div id="fh5co-gallery">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
+          <h2>Os i billeder</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup image-popup" href="gallery-images-story/tidligt.jpg"><img src="gallery-images-story/tidligt.jpg" class="img-responsive" ></a>
+          </div>
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images-story/halloween.jpg"><img src="gallery-images-story/halloween.jpg" class="img-responsive"></a>
+          </div>
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images-story/legoland.jpg"><img src="gallery-images-story/legoland.jpg" class="img-responsive"></a>
+          </div>
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images-story/barcelona.jpg"><img src="gallery-images-story/barcelona.jpg" class="img-responsive"></a>
+          </div>
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images-story/engaged.jpeg"><img src="gallery-images-story/engaged.jpeg" class="img-responsive"></a>
+          </div>
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images-story/ferie2.jpeg"><img src="gallery-images-story/ferie2.jpeg" class="img-responsive"></a>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images-story/afslutning.jpg"><img src="gallery-images-story/afslutning.jpg" class="img-responsive"></a>
+          </div>
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images-story/jul2.jpeg"><img src="gallery-images-story/jul2.jpeg" class="img-responsive"></a>
+          </div>
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images-story/invitation.jpg"><img src="gallery-images-story/invitation.jpg" class="img-responsive"></a>
+          </div>
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images-story/bio.jpeg"><img src="gallery-images-story/bio.jpeg" class="img-responsive"></a>
+          </div>
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images-story/car.jpeg"><img src="gallery-images-story/car.jpeg" class="img-responsive"></a>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images-story/jul.jpg"><img src="gallery-images-story/jul.jpg" class="img-responsive"></a>
+          </div>
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images-story/newyear.jpg"><img src="gallery-images-story/newyear.jpg" class="img-responsive"></a>
+          </div>
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images-story/ferie.jpg"><img src="gallery-images-story/ferie.jpg" class="img-responsive"></a>
+          </div>
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images-story/teater.jpg"><img src="gallery-images-story/teater.jpg" class="img-responsive"></a>
+          </div>
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images-story/cph.jpeg"><img src="gallery-images-story/cph.jpeg" class="img-responsive"></a>
+          </div>
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images-story/ferie3.jpeg"><img src="gallery-images-story/ferie3.jpeg" class="img-responsive"></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="fh5co-gallery">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
+          <h2>Bryllup's forberedelser</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup image-popup" href="gallery-images-prep/programmering.jpg"><img src="gallery-images-prep/programmering.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images-prep/invitationer.jpg"><img src="gallery-images-prep/invitationer.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images-prep/magnet.jpg"><img src="gallery-images-prep/magnet.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div id="fh5co-gallery">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
+          <h2>Bryllup's galleriet</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup image-popup" href="gallery-images/gallery-1.jpg"><img src="gallery-images/gallery-1.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
+          </div>
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images/gallery-7.jpg"><img src="gallery-images/gallery-7.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images/gallery-6.jpg"><img src="gallery-images/gallery-6.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
+          </div>
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images/gallery-2.jpg"><img src="gallery-images/gallery-2.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
+          </div>
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images/gallery-5.jpg"><img src="gallery-images/gallery-5.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images/gallery-3.jpg"><img src="gallery-images/gallery-3.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
+          </div>
+          <div class="gallery animate-box">
+            <a class="gallery-img image-popup" href="gallery-images/gallery-4.jpg"><img src="gallery-images/gallery-4.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php include 'includes/footer.php'; ?>
+</body>
 </html>
 
