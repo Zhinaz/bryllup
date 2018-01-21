7d164e08b0e8ad7e95a579e9c728c1d87d3a6165
Tobias Hvass Mølbak <tobias@molbak.org>
2018-01-21 14:04:28 +0100

    Guestlist built + Bestman image

--- guest.php
+++ guest.php
@@ -39,35 +39,95 @@
           </div>
       </div>-->
       <div class="row row-bottom-padded-lg">
-        <div class="col-md-3 text-center animate-box">
+        <div class="col-md-4 text-center animate-box">
           <div class="groom-men">
             <img src="images/groom-men-1.jpg" class="img-responsive">
             <h3>Sebastian Rosenørn De Lasson</h3>
             <h2>Forlover</h2>
           </div>
         </div>
-        <!--<div class="col-md-3 text-center animate-box">
-          <div class="groom-men">
+        <div class="col-md-4 text-center animate-box">
+          <div class="bridesmaid">
             <img src="images/groom-men-2.jpg" class="img-responsive">
-            <h3>Mike Paterson</h3>
+            <h3>... er ved at blive spurgt</h3>
             <h2>Brudepige</h2>
           </div>
         </div>
-        <div class="col-md-3 text-center animate-box">
+        <!--<div class="col-md-3 text-center animate-box">
           <div class="groom-men">
             <img src="images/groom-men-3.jpg" class="img-responsive">
             <h3>Brench Thompson</h3>
             <h2>Brudesvend</h2>
           </div>
-        </div>
-        <div class="col-md-3 text-center animate-box">
+        </div>-->
+        <div class="col-md-4 text-center animate-box">
           <div class="groom-men">
             <img src="images/groom-men-4.jpg" class="img-responsive">
-            <h3>Blake Haste</h3>
+            <h3>... er ved at blive spurgt</h3>
             <h2>Toastmaster</h2>
           </div>
-        </div>-->
+        </div>
       </div>
+      
+      <div class="row animate-box">
+          <div class="col-md-8 col-md-offset-2 text-center heading-section">
+              <h2>Gæstelisten</h2>
+          </div>
+      </div>
+      
+      <div class="row animated-box col-md-offset-3">
+        <ul class="list-group col-md-6 ">
+          <li class="list-group-item borderless">Jesper Mølbak</li>
+          <li class="list-group-item borderless">Vibeke Hvass Mølbak</li>
+          <li class="list-group-item borderless">Victor Hvass Mølbak</li>
+          <li class="list-group-item borderless">Henning Madsen</li>
+          <li class="list-group-item borderless">Kirsten Mølbak</li>
+          <li class="list-group-item borderless">Ole Lentz</li>
+          <li class="list-group-item borderless">Lotte Hvass Lentz</li>
+          <li class="list-group-item borderless">Emil Lentz</li>
+          <li class="list-group-item borderless">Laura Lentz</li>
+          <li class="list-group-item borderless">Torsten Pedersen</li>
+          <li class="list-group-item borderless">Mette Pedersen</li>
+          <li class="list-group-item borderless">Thomas Pedersen</li>
+          <li class="list-group-item borderless">Christian Pedersen</li>
+          <li class="list-group-item borderless">Lars Pedersen</li>
+          <li class="list-group-item borderless">Sebastian Rosenørn De Lasson</li>
+          <li class="list-group-item borderless">Rasmus Vollstedt</li>
+          <li class="list-group-item borderless">Jacob Andersen</li>
+          <li class="list-group-item borderless">Lone Jørgensen</li>
+          <li class="list-group-item borderless">Joakim Iversen</li>
+          <li class="list-group-item borderless">Nicoline Cecilie Vestergaard</li>
+          <li class="list-group-item borderless">Kristoffer Nygård Jensen</li>
+          <li class="list-group-item borderless">Anna Catharina Danielsen</li>
+          <li class="list-group-item borderless">Camilla Sander Christensen</li>
+        </ul>
+        <ul class="list-group col-md-6 ">
+          <li class="list-group-item borderless">Per Ettrup Christensen</li>
+          <li class="list-group-item borderless">Susanne Christensen</li>
+          <li class="list-group-item borderless">Matis Ettrup Christensen</li>
+          <li class="list-group-item borderless">Theis Ettrup Christensen</li>
+          <li class="list-group-item borderless">Frederikke Nielsen</li>
+          <li class="list-group-item borderless">Ann Etrrup Chrisenten</li>
+          <li class="list-group-item borderless">Hans Jørgen Christensen</li>
+          <li class="list-group-item borderless">Ingrid Kirkegaard Christensen</li>
+          <li class="list-group-item borderless">Ida Kirkegaard Christensen</li>
+          <li class="list-group-item borderless">Eskild Nilas Laursen</li>
+          <li class="list-group-item borderless">Yrsa</li>
+          <li class="list-group-item borderless">Niels Peter Andersen</li>
+          <li class="list-group-item borderless">Mikkel Andersen</li>
+          <li class="list-group-item borderless">Kasper Andersen</li>
+          <li class="list-group-item borderless">Anja Mølbæk Bagge</li>
+          <li class="list-group-item borderless">Monica Topholm</li>
+          <li class="list-group-item borderless">Jannick Kjær Jørgensen</li>
+          <li class="list-group-item borderless">Kristine Bitcsh</li>
+          <li class="list-group-item borderless">Thomas Vigh Sørensen</li>
+          <li class="list-group-item borderless">Kim Marturin Christensen</li>
+          <li class="list-group-item borderless">Anja Bach Christensen</li>
+          <li class="list-group-item borderless">Dion Kær Andersen</li>
+          <li class="list-group-item borderless">Betül Özkaya</li>
+        </ul>
+      </div>
+      
     </div>
   </div>
 
