18a069fe93ca567212d6f4e0c4135b057d862381
Tobias Hvass Mølbak <tobias@molbak.org>
2018-03-18 13:16:44 +0100

    Updated wishlist

--- wishlist.php
+++ wishlist.php
@@ -34,10 +34,36 @@
         </div>
       </div>
       
+      <!--
       <div class="row animate-box">
           <div class="col-md-8 col-md-offset-2 text-center">
               <h1>... er ved at blive skrevet</h1>
           </div>
+      </div>
+      -->
+      <div class="row animated-box col-md-offset-3">
+        <ul class="list-group">
+          <li class="list-group-item borderless">Eva Trio gryde - 2.5L (Dura line) - <a href="https://www.evasolo.com/da/kokken/gryder-og-pander/gryder/dura-line/256320/">Ses her</a></li>
+          <li class="list-group-item borderless">Eva Trio gryde - 3.8L (Dura line) - <a href="https://www.evasolo.com/da/kokken/gryder-og-pander/gryder/dura-line/256332/">Ses her</a></li>
+          <li class="list-group-item borderless">Eva Trio kasserolle - 1.8L (Dura line) - <a href="https://www.evasolo.com/da/kokken/gryder-og-pander/kasseroller/dura-line/256318/">Ses her</a></li>
+          <li class="list-group-item borderless">De tre ovenstående gryder <a href="https://www.evasolo.com/da/kokken/gryder-og-pander/grydesat/dura-line/256372/">findes også som sæt</a></li>
+          <li class="list-group-item borderless">Eva Trio glaslåg - 16cm - <a href="https://www.evasolo.com/da/kokken/gryder-og-pander/lag/tallerkenlag-m-glas/201016/">Ses her</a></li>
+          <li class="list-group-item borderless">Eva Trio glaslåg - 20cm - <a href="https://www.evasolo.com/da/kokken/gryder-og-pander/lag/tallerkenlag-m-glas/201020/">Ses her</a></li>
+          <li class="list-group-item borderless">Eva Trio hældelåg - 16cm - <a href="https://www.evasolo.com/da/kokken/gryder-og-pander/lag/haldelag/203016/">Ses her</a></li>
+          <li class="list-group-item borderless">Eva Trio stegepande - 24cm (Dura line) - <a href="https://www.evasolo.com/da/kokken/gryder-og-pander/stegepander/dura-line/256124/">Ses her</a></li>
+          <li class="list-group-item borderless">Eva Trio stegepande - 28cm (Dura line) - <a href="https://www.evasolo.com/da/kokken/gryder-og-pander/stegepander/dura-line/256128/">Ses her</a></li>
+          <li class="list-group-item borderless">Eva Solo Ovnfast fad - 30.5cm - <a href="https://www.imerco.dk/borddaekning/skaale-og-fade/ovnfaste-fade/eva-trio-ovnfast-fad-305-cm?id=100362415">Ses her</a></li>
+          <li class="list-group-item borderless">Bitz Bestik 16 dele - Sort - <a href="https://www.bitzshop.dk/bestik/bestiksaet/product/bitz-bestik16-dele-sort">Ses her</a></li>
+          <li class="list-group-item borderless">Royal Copenhagen Middagstallerken - Hvid riflet 27cm - <a href="https://www.imerco.dk/borddaekning/stel/tallerkener/royal-copenhagen-hvid-riflet-middagstallerken-27-cm-1-stk?id=100081580">Ses her</a></li>
+          <li class="list-group-item borderless">Royal Copenhagen Flora - 22cm - <a href="https://www.royalcopenhagen.com/dk/da/Products/Plates/c/Plates#filter={&quot;Series&quot;:[&quot;Flora&quot;],&quot;Dimensions&quot;:[&quot;22 cm&quot;]}&term=">Ses her</a></li>
+          <li class="list-group-item borderless">Raw Middagstallerken - Nude 28cm - <a href="https://www.imerco.dk/borddaekning/stel/tallerkener/raw-middagstallerken-28-cm-1-stk?id=100361458">Ses her</a> - Kun en tallerken ønskes</li>
+          <li class="list-group-item borderless">Palletkniv - Gerne med stort hoved</li>
+          <li class="list-group-item borderless">Natbordslamper - Med fod, 2 stks</li>
+          <li class="list-group-item borderless">Bagagevægt</li>
+          <li class="list-group-item borderless">Billetter til 'Kærlighed ved første hik' musical - 2 stks - <a href="https://www.hikmusical.dk/">Ses her</a></li>
+          <li class="list-group-item borderless">Penge til spisebordsstole</li>
+          <li class="list-group-item borderless">Penge til bryllupsrejse</li>
+        </ul>
       </div>
       
     </div>
